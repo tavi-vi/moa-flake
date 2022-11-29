c3d23a29b033afba9cd83d5423b0d12bebbba083
Tavi <syncopegirl@gmail.com>
2022-11-29 09:04:36 -0500

    attempt to fix id int-string bug
    The id is returned as an integer for mastodon and postgres doesn't like that.

--- flake.nix
+++ flake.nix
@@ -48,6 +48,7 @@
             chmod -R +w "$out"/logs
             rm -r "$out"/logs
             ln -s /etc/moa.conf "$out"/config.py
+            patch -u "$out"/app.py -i app-id.patch
           '';
         };
       }
